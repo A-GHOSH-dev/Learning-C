44f34a2c18a212dc0f65814666f189e1f1d009dd
A-GHOSH-dev <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-03-05 17:36:06 +0530

    Add files via upload

---
+++ operators8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main(){
+
+    int x,y,z,p,q,r,s,t;
+
+    printf("Enter the two numbers: ");
+    scanf("%d%d",&x,&y);
+
+    printf("\n%d",x&y);
+    printf("\n%d",x|y);
+
+
+
+
+
+}
+
