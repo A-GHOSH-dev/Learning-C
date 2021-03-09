b8184541f636d5e03ffbea579f09d535ea10d87e
A-GHOSH-dev <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-03-09 18:42:32 +0530

    Add files via upload

---
+++ for1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+
+for (i=1; i<=10; i++)
+{
+printf("i = %d \n", i);
+}
+}
+
+
+//i = 1
+//i = 2
+//i = 3
+//i = 4
+//i = 5
+//i = 6
+//i = 7
+//i = 8
+//i = 9
+//i = 10
