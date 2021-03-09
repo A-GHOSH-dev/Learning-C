6bc031c42501d8acdfc692c247879a87bbff1ee8
A-GHOSH-dev <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-03-09 18:35:44 +0530

    Add files via upload

---
+++ nesteloop1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int i=0, j=0;
+    while (++i <= 5)
+    {
+        while (++j <= 5)
+        {
+            printf("%d\n",j);
+        }
+        printf("%d\n",j);
+    }
+}
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//10
